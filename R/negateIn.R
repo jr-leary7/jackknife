40364f01cd8408a237961e7e294691f84a2a3afb
Jack Leary <jrleary@live.unc.edu>
2020-03-24 18:40:23 -0400

    added %in% negation function

---
+++ R/negateIn.R
@@ -0,0 +1,9 @@
+#' A function to negate the %in% operator.
+#'
+#' This function returns the complement / inverse of the %in% operator. Useful for subsetting, etc.
+#'
+#' @export
+#' @example
+#' df <- df["df$var %!in% "pattern", ]
+
+`%!in%` <- Negate(`%in%`)
