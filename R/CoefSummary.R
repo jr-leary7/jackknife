139c983b1c9fa671a68c09adab71dd6afd9a7135
jr-leary7 <jrleary@live.unc.edu>
2021-03-18 21:38:03 -0400

    Model coefficient summary function

---
+++ R/CoefSummary.R
@@ -0,0 +1,44 @@
+#' A function to provide a summary of a linear model.
+#'
+#' This function takes in the output from lm() or glm() and provides a tidy, aesthetically-pleasing summary of the coefficients.
+#' @param model The model to be analyzed. Defaults to NULL.
+#' @param conf.level The confidence level for determining variable significance. Defaults to 95% aka 0.05.
+#' @param kable Should the table be returned as an interactive kable instead of a simple dataframe? Defaults to TRUE.
+#' @param font What font should be used? Defaults to "Avenir".
+#' @param kable.caption Define a caption to be placed above the table. Defaults to "Model Coefficients".
+#' @param table.digits The number of digits to round to. Defaults to 4.
+#' @import dplyr
+#' @import broom
+#' @import kableExtra
+#' @export
+#' @examples
+#' CoefSummary(model = my_glm, font = "Georgia")
+
+CoefSummary <- function (model = NULL,
+                         kable = TRUE,
+                         conf.level = 0.05,
+                         font = "Avenir",
+                         kable.caption = NULL,
+                         table.digits = 4) {
+  # check parameters
+  if (is.null(model)) stop("You forgot to provide a model.")
+  # summarize data
+  model %>%
+    tidy() %>%
+    rename(Variable = term, Beta = estimate, `Std. Error` = std.error, `T-statistic` = statistic, `P-value` = p.value) %>%
+    mutate(Significant = ifelse(`P-value` < conf.level, "*", "")) -> coef_temp
+  # exponentiate coefficients if criteria are met
+  if ("glm" %in% class(model) && family(model)$link == "log") {
+    coef_temp %>% mutate(`Exp(Beta)` = exp(Beta)) -> coef_temp
+  }
+  # create kable output if desired
+  if (kable) {
+    coef_temp %>%
+      kbl(digits = table.digits, booktabs = TRUE, caption = kable.caption) %>%
+      kable_minimal("hover", full_width = FALSE, html_font = font) -> final_table
+  } else {
+    final_table <- coef_temp
+  }
+  return(final_table)
+}
+
